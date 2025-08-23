4eaa6b898be2597e0e8bd51bc72362d04b38a5c7
AndrewKelton <andrew7kelton@gmail.com>
2025-08-22 21:51:26 -0400

    chore: update script

---
+++ js/script.js
@@ -0,0 +1,85 @@
+// script.js
+
+function toggleDetails(id, headerElement) {
+  const details = document.getElementById(id);
+  const icon = headerElement.querySelector('.toggle-icon');
+  const isVisible = details.style.display === "block";
+
+  details.style.display = isVisible ? "none" : "block";
+  icon.classList.toggle("rotate", !isVisible);
+
+  if (!isVisible) {
+    setTimeout(() => {
+    const rect = details.getBoundingClientRect();
+    const scrollThreshold = 50;
+    const windowHeight = window.innerHeight;
+
+    if (rect.bottom > windowHeight - scrollThreshold) {
+      window.scrollBy({
+        top: rect.bottom - (windowHeight - scrollThreshold) + 20,
+        behavior: 'smooth'
+      });
+    } else if (rect.top < scrollThreshold * 2.2) {
+      window.scrollBy({
+        top: -(rect.top + scrollThreshold),
+        behavior: 'smooth'
+      });
+    }
+    }, 300);
+  }
+}
+
+// Function to toggle dropdown visibility
+function toggleDropdown(dropdownId) {
+  var dropdown = document.getElementById(dropdownId);
+  dropdown.classList.toggle("show");
+  console.log('Dropdown toggled: ', dropdown);
+
+  if (dropdown.classList.contains("show")) {
+    var dropdownRect = dropdown.getBoundingClientRect();
+    var dropdownBot = dropdownRect.bottom;  // Dropdown's distance from the bottom of the viewport
+    var dropdownTop = dropdownRect.top; // Dropdown's distance from the top of the viewport
+    var windowHeight = window.innerHeight;  // Viewport height
+    var scrollThreshold = 50;
+
+    // Scroll dynamically based on the dropdown's bottom position
+    setTimeout(function () {
+      if (dropdownBot > windowHeight - scrollThreshold) {
+        // Scroll down if the dropdown is too low on the screen
+        window.scrollBy({
+          top: dropdownBot - (windowHeight - scrollThreshold) + 20,
+          behavior: 'smooth'
+        });
+        console.log('scrolled down');
+      } else if (dropdownTop < scrollThreshold * 2.2) {
+        // Scroll up if the dropdown is too high on the screen
+        window.scrollBy({
+          top: -(dropdownTop + scrollThreshold),
+          behavior: 'smooth'
+        });
+        console.log('scrolled up');
+      }
+    }, 300);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  console.log("Observer initialized"); 
+  const faders = document.querySelectorAll(".fade-in-section");
+
+  const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px 0px -10% 0px"
+  };
+  const appearOnScroll = new IntersectionObserver((entries, observer) => {
+    entries.forEach(entry => {
+      if (!entry.isIntersecting) return;
+      entry.target.classList.add("visible");
+      observer.unobserve(entry.target);
+    });
+  }, appearOptions);
+
+  faders.forEach(section => {
+    appearOnScroll.observe(section);
+  });
+});
